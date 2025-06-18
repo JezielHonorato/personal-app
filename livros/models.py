bdcdfb9de467ccbe7736a4694195fe17bec72137
Jeziel Honorato <jezielh.ss@gmail.com>
2025-06-17 21:39:50 -0300

    Autor

--- livros/models.py
+++ livros/models.py
@@ -12,3 +12,16 @@ class Pais(models.Model):
         verbose_name_plural = "Países"
         ordering = ['nome']
         
+class Autor(models.Model):
+    nome = models.CharField(max_length=255, verbose_name="Nome")
+    data_nascimento = models.DateField(blank=True, null=True, verbose_name="Nascimento")
+    data_morte = models.DateField(blank=True, null=True, verbose_name="Falecimento")
+    pais = models.ForeignKey(Pais, on_delete=models.SET_NULL, blank=True, null=True, related_name='autores', verbose_name="País de Origem")
+    
+    def __str__(self):
+        return self.nome
+
+    class Meta:
+        verbose_name = "Autor"
+        verbose_name_plural = "Autores"
+        ordering = ['nome']
