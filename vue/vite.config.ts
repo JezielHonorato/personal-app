b41a99a6124e014eb85fb92ab4c0db250f222e01
Jeziel Honorato <jezielh.ss@gmail.com>
2025-06-22 17:59:51 -0300

    Update vite.config.ts

--- vue/vite.config.ts
+++ vue/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { fileURLToPath } from 'url';
+import * as path from 'path';
+import tailwindcss from '@tailwindcss/vite';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  plugins: [vue(), tailwindcss(),], 
-})
+	plugins: [vue(), tailwindcss()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, 'src'),
+		},
+	},
+});
