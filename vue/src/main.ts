66351327d9e071ce39aa24b22504b79b194d03f5
Jeziel Honorato <jezielh.ss@gmail.com>
2025-06-23 22:06:50 -0300

    Update main.ts

--- vue/src/main.ts
+++ vue/src/main.ts
@@ -1,5 +1,17 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import router from './router';
+import App from './App.vue';
+import { useSettingsStore } from './stores/useSettingsStore';
 import './style.css';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+const pinia = createPinia();
+
+app.use(router);
+app.use(pinia);
+
+const settingsStore = useSettingsStore();
+settingsStore.initializeSettings();
+
+app.mount('#app');
