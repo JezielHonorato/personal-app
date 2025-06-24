8c1bc6dcc1667fc674158bf9e2cd5806c7d50739
Jeziel Honorato <jezielh.ss@gmail.com>
2025-06-24 09:21:53 -0300

    Update serializers.py

--- django/livros/serializers.py
+++ django/livros/serializers.py
@@ -5,61 +5,48 @@ from .models import Pais, Autor, Genero, Livro
 class PaisSerializer(serializers.ModelSerializer):
     class Meta:
         model = Pais
-        fields = "__all__"
+        fields = ["id", "nome"]
 
 
 class AutorSerializer(serializers.ModelSerializer):
     pais = PaisSerializer(read_only=True)
+    pais_id = serializers.PrimaryKeyRelatedField(
+        queryset=Pais.objects.all(), source="pais", write_only=True, required=False
+    )
 
     class Meta:
         model = Autor
-        fields = "__all__"
+        fields = ["id", "nome", "data_nascimento", "data_obito", "pais", "pais_id"]
 
 
 class GeneroSerializer(serializers.ModelSerializer):
     class Meta:
         model = Genero
-        fields = "__all__"
+        fields = ["id", "nome"]
 
 
 class LivroSerializer(serializers.ModelSerializer):
     autor = AutorSerializer(read_only=True)
-    genero = GeneroSerializer(read_only=True)
-
     autor_id = serializers.PrimaryKeyRelatedField(
-        queryset=Autor.objects.all(),
-        source="autor",
-        write_only=True,
-        allow_null=True,
+        queryset=Autor.objects.all(), source="autor", write_only=True, required=False
     )
+    genero = GeneroSerializer(read_only=True)
     genero_id = serializers.PrimaryKeyRelatedField(
-        queryset=Genero.objects.all(),
-        source="genero",
-        write_only=True,
-        allow_null=True,
+        queryset=Genero.objects.all(), source="genero", write_only=True, required=False
     )
 
     class Meta:
         model = Livro
-        fields = (
+        fields = [
             "id",
             "titulo",
             "titulo_original",
-            "autor",
-            "genero",
             "ano_publicacao",
-            "status",
-            "avaliacao",
-            "data_inicio",
-            "data_fim",
+            "lido",
             "arquivo",
             "capa",
-            "data_criacao",
-            "data_atualizacao",
+            "autor",
             "autor_id",
+            "genero",
             "genero_id",
-        )
-        read_only_fields = (
-            "data_criacao",
-            "data_atualizacao",
-        )
+        ]
