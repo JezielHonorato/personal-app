08d213b1aac8d275c595c2eacb0920c33002581d
Jeziel Honorato <jezielh.ss@gmail.com>
2025-06-25 17:56:01 -0300

    Update apiModels.ts

--- vue/src/types/apiModels.ts
+++ vue/src/types/apiModels.ts
@@ -1,35 +1,33 @@
 export interface Pais {
-	id: number;
-	nome: string;
+    id: number;
+    nome: string;
 }
 
 export interface Autor {
-	id: number;
-	nome: string;
-	data_nascimento?: string | null;
-	data_obito?: string | null;
-	pais?: Pais;
-	pais_id?: number | null;
+    id: number;
+    nome: string;
+    data_nascimento?: string | null;
+    data_obito?: string | null;
+    pais?: Pais;
+    pais_id?: number | null;
 }
 
 export interface Genero {
-	id: number;
-	nome: string;
+    id: number;
+    nome: string;
 }
 
 export interface Livro {
-	id: number;
-	titulo: string;
-	titulo_original?: string | null;
-	autor?: Autor;
-	genero?: Genero;
-	ano_publicacao?: number | null;
-	status: string;
-	avaliacao?: number | null;
-	data_inicio?: string | null;
-	data_fim?: string | null;
-	arquivo?: string | null;
-	capa?: string | null;
-	data_criacao: string;
-	data_atualizacao: string;
+    id: number;
+    titulo: string;
+    titulo_original?: string | null;
+    autor?: Autor;
+    genero?: Genero;
+    ano_publicacao?: number | null;
+    lido: boolean;
+    arquivo?: string | null;
+    capa?: string | null;
+	autor_id?: Autor;
+    genero_id?: Genero;
+
 }
