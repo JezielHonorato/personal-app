be21afe4cb671972c51ffbffb8ad97ca0a7888e6
Jeziel Honorato <jezielh.ss@gmail.com>
2025-06-26 17:57:33 -0300

    Create FormSubmitButton.vue

---
+++ vue/src/components/form/FormSubmitButton.vue
@@ -0,0 +1,13 @@
+<template>
+    <button type="submit" :disabled="loading" class="w-full sm:w-auto bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 disabled:opacity-50 disabled:cursor-not-allowed transition duration-200 ease-in-out transform hover:-translate-y-0.5 dark:bg-blue-700 dark:hover:bg-blue-600 dark:focus:ring-blue-400">
+        {{ loading ? loadingText : text }}
+    </button>
+</template>
+
+<script setup lang="ts">
+    defineProps<{
+        loading: boolean;
+        text: string;
+        loadingText?: string;
+    }>();
+</script>
