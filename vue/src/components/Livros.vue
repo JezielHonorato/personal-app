bd1f612ad0e82f706d90ccf1d79bb62dac38cccd
Jeziel Honorato <jezielh.ss@gmail.com>
2025-06-26 17:57:17 -0300

    Create Livros.vue

---
+++ vue/src/components/Livros.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="min-h-screen bg-white text-gray-900 dark:bg-gray-900 dark:text-gray-100 transition-colors duration-300">
+        <div class="container mx-auto p-4">
+            <header class="flex justify-between items-center mb-6">
+                <h1 class="text-4xl font-bold text-indigo-700 dark:text-indigo-300">Minha Biblioteca Pessoal</h1>
+            </header>
+
+            <div class="bg-gray-100 dark:bg-gray-800 p-6 rounded-lg shadow-md mb-8">
+                <LivrosPesquisa @pesquisa="pesquisarLivros" />
+            </div>
+
+            <div class="flex justify-center mb-12 mt-8">
+                <RouterLink to="livros/cadastro" class="inline-flex items-center justify-center bg-blue-600 hover:bg-blue-700 text-white font-bold py-3 px-8 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition duration-200 ease-in-out transform hover:-translate-y-0.5 dark:bg-blue-700 dark:hover:bg-blue-600 dark:focus:ring-blue-400">
+                    <Plus class="h-5 w-5 mr-2" />
+                    Cadastrar Novo Livro
+                </RouterLink>
+            </div>
+
+            <div class="bg-gray-100 dark:bg-gray-800 p-6 rounded-lg shadow-md">
+                <LivrosResultado :livros="livros" @livroApagado="removerLivro" />
+                <div v-if="carregandoLivros" class="text-center text-gray-600 dark:text-gray-400 py-4">Carregando livros...</div>
+                <div v-if="errorLivros" class="text-center text-red-500 py-4">Erro ao carregar livros: {{ errorLivros }}</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+    import { Plus } from 'lucide-vue-next';
+    import LivrosPesquisa from '@/components/LivrosPesquisa.vue';
+    import LivrosResultado from '@/components/LivrosResultado.vue';
+    import { useLivros } from '@/composables/useDatabaseLivros';
+
+    const { livros, carregandoLivros, errorLivros, filtroNome, filtroGenero, filtroPais, filtroAnoInicial, filtroAnoFinal } = useLivros();
+
+    const pesquisarLivros = ({ nome, genero, pais, anoInicial, anoFinal }: { nome: string; genero: number | null; pais: number | null; anoInicial: number | null; anoFinal: number | null }) => {
+        filtroNome.value = nome;
+        filtroGenero.value = genero;
+        filtroPais.value = pais;
+        filtroAnoInicial.value = anoInicial;
+        filtroAnoFinal.value = anoFinal;
+    };
+	
+    const removerLivro = (id: number) => {
+        livros.value = livros.value.filter((l) => l.id !== id);
+    };
+</script>
