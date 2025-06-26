f9fa6744eada9ff735138d88939f1d0b0ed7f0f1
Jeziel Honorato <jezielh.ss@gmail.com>
2025-06-26 17:57:19 -0300

    Create Home.vue

---
+++ vue/src/components/Home.vue
@@ -0,0 +1,14 @@
+<template>
+	<div>
+		<h1>Bem vindo</h1>
+		<p>1234567890 - _ ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz</p>
+		<h2>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum voluptatibus unde ut voluptates repellat non voluptatum veniam voluptate, culpa iusto alias dolorem id natus. Sequi saepe magni modi voluptatibus commodi.</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid obcaecati minus velit facilis ipsa accusantium recusandae ullam voluptatem atque deserunt, error, ut reiciendis molestias odit dolore quisquam nihil natus debitis!</p>
+		<strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci maxime quam voluptate nam ullam vitae distinctio excepturi aut beatae fugiat. Dicta mollitia accusamus adipisci perspiciatis in eum voluptatibus earum maxime.</strong>
+	</div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
