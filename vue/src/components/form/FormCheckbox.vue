7a606eb5d5e84e3a7c419f400e5d74e3f12b674a
Jeziel Honorato <jezielh.ss@gmail.com>
2025-06-27 17:57:56 -0300

    Create FormCheckbox.vue

---
+++ vue/src/components/form/FormCheckbox.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="flex items-center">
+        <input type="checkbox" :id="id" :checked="modelValue" @change="$emit('update:modelValue', ($event.target as HTMLInputElement).checked)" class="mr-2" />
+        <label :for="id" class="text-sm font-semibold text-gray-800 dark:text-gray-200">{{ label }}</label>
+    </div>
+</template>
+
+<script setup lang="ts">
+    defineProps<{
+        id: string;
+        label: string;
+        modelValue: boolean | undefined;
+    }>();
+	
+    defineEmits(['update:modelValue']);
+</script>
