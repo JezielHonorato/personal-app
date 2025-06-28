cacbd446676e840403d158bb8656eba41e9997c8
Jeziel Honorato <jezielh.ss@gmail.com>
2025-06-28 19:42:16 -0300

    Update Home.vue

--- vue/src/components/Home.vue
+++ vue/src/components/Home.vue
@@ -1,5 +1,5 @@
 <template>
-	<div>
+	<div class="min-h-screen bg-white text-gray-900 dark:bg-gray-900 dark:text-gray-100">
 		<h1>Bem vindo</h1>
 		<p>1234567890 - _ ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz</p>
 		<h2>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum voluptatibus unde ut voluptates repellat non voluptatum veniam voluptate, culpa iusto alias dolorem id natus. Sequi saepe magni modi voluptatibus commodi.</h2>
