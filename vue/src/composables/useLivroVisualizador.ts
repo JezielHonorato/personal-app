ee5b5ababf81e50a486857a45c2fde325d33d284
Jeziel Honorato <jezielh.ss@gmail.com>
2025-06-28 19:42:02 -0300

    Create useLivroVisualizador.ts

---
+++ vue/src/composables/useLivroVisualizador.ts
@@ -0,0 +1,27 @@
+import { ref } from 'vue';
+import { api } from '@/services';
+import type { ConteudoLivro } from '@/types';
+
+export function useLivroVisualizador() {
+    const data = ref<ConteudoLivro | null>(null);
+    const isLoading = ref(false);
+    const error = ref<string | null>(null);
+
+    const fetchConteudo = async (id: number) => {
+        isLoading.value = true;
+        error.value = null;
+        data.value = null;
+        try {
+            const response = await api.livros.getConteudo(id);
+            data.value = response.data;
+            console.log(data.value);
+        } catch (err: any) {
+            error.value = err.message || 'Falha ao carregar conteúdo do livro.';
+            console.error(err);
+        } finally {
+            isLoading.value = false;
+        }
+    };
+
+    return { data, isLoading, error, fetchConteudo };
+}
