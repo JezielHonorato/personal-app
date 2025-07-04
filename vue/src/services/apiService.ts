da57dda60c2dfdc65ce0e52a96e6075f0fd415d7
JezielHonorato <jezielh.ss@gmail.com>
2025-07-03 23:00:36 -0300

    Update apiService.ts

--- vue/src/services/apiService.ts
+++ vue/src/services/apiService.ts
@@ -14,8 +14,8 @@ export function createApi(baseURL: string) {
         (error) => {
             if (axios.isAxiosError(error) && error.response) {
                 const errorInfo = getErrorMessage(error.response.status);
-                const errorDetails = error.response.data?.detail || error.response.data?.message || JSON.stringify(error.response.data);
-                return Promise.reject(new Error(`${errorInfo.code} - ${errorInfo.message} (Detalhes: ${errorDetails})`));
+                // const errorDetails = error.response.data?.detail || error.response.data?.message || JSON.stringify(error.response.data); (Detalhes: ${errorDetails})
+                return Promise.reject(new Error(`${errorInfo.code} - ${errorInfo.message}`));
             }
             return Promise.reject(error);
         }
