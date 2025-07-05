f1964fc62ba054fa23633c9298d3cb32e35d06ce
JezielHonorato <jezielh.ss@gmail.com>
2025-07-04 21:55:07 -0300

    Create PaisCadastroModal.vue

---
+++ vue/src/components/PaisCadastroModal.vue
@@ -0,0 +1,37 @@
+<template>
+    <Modal :show="show" @close="$emit('close')" title="Cadastrar Novo País">
+        <form @submit.prevent="handleSubmit">
+            <div class="mb-4">
+                <FormTextInput id="paisNome" label="Nome do País" placeholder="Ex: Brasil" v-model="paisForm.nome" />
+            </div>
+            <p v-if="submitError" class="text-red-500 text-sm mb-4">{{ submitError }}</p>
+            <FormSubmitButton :loading="isSubmitting" text="Salvar País" loadingText="Salvando..." />
+        </form>
+    </Modal>
+</template>
+
+<script setup lang="ts">
+    import { ref } from 'vue';
+    import Modal from './Modal.vue';
+    import { FormTextInput, FormSubmitButton } from '@/components/form';
+    import { usePaises } from '@/composables/useDatabaseLivros';
+    import type { PaisForm } from '@/types';
+
+    defineProps<{ show: boolean }>();
+    const emit = defineEmits(['close', 'paisCriado']);
+
+    const { create: createPais, error: submitError, carregando: isSubmitting } = usePaises();
+    const paisForm = ref<PaisForm>({ nome: '' });
+
+    const handleSubmit = async () => {
+        if (!paisForm.value.nome) return;
+        try {
+            await createPais.create(paisForm.value);
+            emit('paisCriado');
+            emit('close');
+            paisForm.value.nome = ''; // Limpa o formulário
+        } catch (e) {
+            // O erro já é tratado pelo 'submitError'
+        }
+    };
+</script>
