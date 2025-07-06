086ddfaa94fb1f8ec15c96c7fcb658e85c74e0a3
JezielHonorato <jezielh.ss@gmail.com>
2025-07-05 22:46:53 -0300

    Update tableGenero.ts

--- vue/src/services/databaseLivro/tableGenero.ts
+++ vue/src/services/databaseLivro/tableGenero.ts
@@ -3,8 +3,8 @@ import type { Genero, GeneroForm } from '@/types';
 
 export function tableGenero(api: AxiosInstance) {
     return {
-        getAll: (params?: Record<string, any>) => api.get<Genero[]>('generos/', { params }),
-        get: (id: number) => api.get<Genero>(`generos/${id}`),
+		get: (id: number) => api.get<Genero>(`generos/${id}`),
+        getFilter: (params?: Record<string, any>) => api.get<Genero[]>('generos/', { params }),
         create: (data: GeneroForm) => api.post<Genero>('generos/', data),
         update: (id: number, data: GeneroForm) => api.put<Genero>(`generos/${id}/`, data),
         delete: (id: number) => api.delete(`generos/${id}/`),
