51c2a4fda20a34d76d1734c7a1fa2a7890e5014d
JezielHonorato <jezielh.ss@gmail.com>
2025-07-05 22:46:56 -0300

    Update tableAutor.ts

--- vue/src/services/databaseLivro/tableAutor.ts
+++ vue/src/services/databaseLivro/tableAutor.ts
@@ -3,8 +3,8 @@ import type { Autor, AutorForm } from '@/types';
 
 export function tableAutor(api: AxiosInstance) {
     return {
-        getAll: (params?: Record<string, any>) => api.get<Autor[]>('autores/', { params }),
-        get: (id: number) => api.get<Autor>(`autores/${id}`),
+		get: (id: number) => api.get<Autor>(`autores/${id}`),
+        getFilter: (params?: Record<string, any>) => api.get<Autor[]>('autores/', { params }),
         create: (data: AutorForm) => api.post<Autor>('autores/', data),
         update: (id: number, data: AutorForm) => api.put<Autor>(`autores/${id}/`, data),
         delete: (id: number) => api.delete(`autores/${id}/`),
