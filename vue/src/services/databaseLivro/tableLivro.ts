71175c482d63bb02c403267b12458a476d6657bf
JezielHonorato <jezielh.ss@gmail.com>
2025-07-05 22:46:51 -0300

    Update tableLivro.ts

--- vue/src/services/databaseLivro/tableLivro.ts
+++ vue/src/services/databaseLivro/tableLivro.ts
@@ -3,8 +3,8 @@ import type { Livro, LivroForm, ConteudoLivro } from '@/types';
 
 export function tableLivro(api: AxiosInstance) {
     return {
-        getAll: (params: Record<string, any>) => api.get<Livro[]>('livros/', { params }),
-        get: (id: number) => api.get<Livro>(`livros/${id}/`),
+		get: (id: number) => api.get<Livro>(`livros/${id}/`),
+        getFilter: (params: Record<string, any>) => api.get<Livro[]>('livros/', { params }),
         getConteudo: (id: number) => api.get<ConteudoLivro>(`livros/${id}/conteudo/`),
         create: (data: LivroForm) => api.post<Livro>('livros/', data, { headers: { 'Content-Type': 'multipart/form-data' } }),
         update: (id: number, data: LivroForm) => api.put<Livro>(`livros/${id}/`, data, { headers: { 'Content-Type': 'multipart/form-data' } }),
