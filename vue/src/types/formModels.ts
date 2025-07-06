c06706ef093f94c78d91218ab858b51a5e8c5fca
JezielHonorato <jezielh.ss@gmail.com>
2025-07-05 22:46:47 -0300

    Update formModels.ts

--- vue/src/types/formModels.ts
+++ vue/src/types/formModels.ts
@@ -1,22 +1,22 @@
 export interface AutorForm {
     id?: number;
     nome: string;
-    data_nascimento?: number | null;
-    data_obito?: number | null;
-    pais_id?: number | null;
+    ano_nascimento: number | null;
+    ano_obito: number | null;
+    pais_id: number | null;
 }
 
 export interface LivroForm {
     id?: number;
     titulo: string;
-    titulo_original?: string | null;
-    autor_id?: number | null;
-    genero_id?: number | null;
-    ano_publicacao?: number | null;
-    lido?: boolean;
-    arquivo_file?: File | null;
-    capa_file?: File | null;
-    capa_url_preview?: string | null;
+    titulo_original: string | null;
+    autor_id: number | null;
+    genero_id: number | null;
+    ano_publicacao: number | null;
+    lido: boolean;
+    arquivo_file: File | null;
+    capa_file: File | null;
+    capa_url_preview: string | null;
 }
 
 export interface PaisForm {
