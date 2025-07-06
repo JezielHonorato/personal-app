7cfa9bac51ccd3f6205df2c05528bfa21a0c5b3f
JezielHonorato <jezielh.ss@gmail.com>
2025-07-05 22:46:49 -0300

    Update tablePais.ts

--- vue/src/services/databaseLivro/tablePais.ts
+++ vue/src/services/databaseLivro/tablePais.ts
@@ -3,8 +3,8 @@ import type { Pais, PaisForm } from '@/types';
 
 export function tablePais(api: AxiosInstance) {
     return {
-        getAll: (params?: Record<string, any>) => api.get<Pais[]>('paises/', { params }),
-        get: (id: number) => api.get<Pais>(`paises/${id}`),
+		get: (id: number) => api.get<Pais>(`paises/${id}`),
+        getFlter: (params?: Record<string, any>) => api.get<Pais[]>('paises/', { params }),
         create: (data: PaisForm) => api.post<Pais>('paises/', data),
         update: (id: number, data: PaisForm) => api.put<Pais>(`paises/${id}/`, data),
         delete: (id: number) => api.delete(`paises/${id}/`),
