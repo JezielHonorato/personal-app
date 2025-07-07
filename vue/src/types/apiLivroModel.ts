e72c3c5c05452875907525170ceee786490c91c8
JezielHonorato <jezielh.ss@gmail.com>
2025-07-06 22:48:14 -0300

    Update apiLivroModel.ts

--- vue/src/types/apiLivroModel.ts
+++ vue/src/types/apiLivroModel.ts
@@ -6,10 +6,10 @@ export interface Pais {
 export interface Autor {
     id: number;
     nome: string;
-    data_nascimento?: number | null;
-    data_obito?: number | null;
-    pais?: Pais;
-    pais_id?: number | null;
+    ano_nascimento: number | null;
+    ano_obito: number | null;
+    pais: Pais;
+    pais_id: number | null;
 }
 
 export interface Genero {
@@ -20,14 +20,14 @@ export interface Genero {
 export interface Livro {
     id: number;
     titulo: string;
-    titulo_original?: string | null;
-    autor?: Autor;
-    genero?: Genero;
-    ano_publicacao?: number | null;
+    titulo_original: string | null;
+    autor: Autor;
+    genero: Genero;
+    ano_publicacao: number | null;
     lido: boolean;
-    arquivo?: string | null;
-    capa?: string | null;
-	autor_id?: Autor;
-    genero_id?: Genero;
+    arquivo: string | null;
+    capa: string | null;
+	autor_id: Autor;
+    genero_id: Genero;
 
 }
