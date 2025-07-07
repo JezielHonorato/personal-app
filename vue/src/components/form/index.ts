25f5de396d00d73da70c606bb34ede8377620cef
JezielHonorato <jezielh.ss@gmail.com>
2025-07-06 22:48:20 -0300

    Update index.ts

--- vue/src/components/form/index.ts
+++ vue/src/components/form/index.ts
@@ -1,6 +1,8 @@
 export { default as FormCheckbox } from '@/components/form/FormCheckbox.vue';
 export { default as FormFileInput } from '@/components/form/FormFileInput.vue';
 export { default as FormNumberInput } from '@/components/form/FormNumberInput.vue';
+export { default as FormRangeDuplo } from '@/components/form/FormRangeDuplo.vue';
+export { default as FormSearchInput } from '@/components/form/FormSearchInput.vue';
 export { default as FormSelect } from '@/components/form/FormSelect.vue';
 export { default as FormSubmitButton } from '@/components/form/FormSubmitButton.vue';
 export { default as FormTextInput } from '@/components/form/FormTextInput.vue';
