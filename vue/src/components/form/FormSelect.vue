019dac795d01b099ff9a69694dcd2f6ebe7efdb1
JezielHonorato <jezielh.ss@gmail.com>
2025-07-07 22:30:49 -0300

    Update FormSelect.vue

--- vue/src/components/form/FormSelect.vue
+++ vue/src/components/form/FormSelect.vue
@@ -2,7 +2,7 @@
     <div>
         <label :for="id" class="block text-gray-800 text-sm font-semibold mb-2 dark:text-gray-200">{{ label }}</label>
         <select :id="id" :value="modelValue" @change="$emit('update:modelValue', ($event.target as HTMLSelectElement).value)" class="shadow-sm border border-gray-300 rounded-md w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-200 ease-in-out dark:bg-gray-700 dark:border-gray-600 dark:text-gray-100">
-            <option disabled :selected="modelValue === null" value="">{{ placeholder }}</option>
+            <option :selected="modelValue === null" value="">{{ placeholder }}</option>
             <option v-for="item in options" :key="item.id" :value="item.id">{{ item.nome }}</option>
         </select>
 
