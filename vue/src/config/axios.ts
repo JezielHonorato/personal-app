9133e6a09b89f36195c8e8be0b4845379fcab04f
JezielHonorato <jezielh.ss@gmail.com>
2025-07-08 23:41:41 -0300

    Create axios.ts

---
+++ vue/src/config/axios.ts
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import { getErrorMessage } from '@/constants/errorMessages';
+
+export const api = axios.create({
+    baseURL: 'http://127.0.0.1:8000/api/',
+    headers: { 'Content-Type': 'application/json' },
+});
+
+api.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        if (axios.isAxiosError(error) && error.response) {
+            const backendMessage = error.response.data?.message;
+
+            if (backendMessage) {
+                return Promise.reject(new Error(backendMessage));
+            }
+
+            const genericError = getErrorMessage(error.response.status);
+            return Promise.reject(new Error(genericError.message));
+        }
+
+        return Promise.reject(error);
+    }
+);
