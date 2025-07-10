aa016080f39cb1a805bdfc12196dc996972f74b4
JezielHonorato <jezielh.ss@gmail.com>
2025-07-09 23:33:56 -0300

    Create apiLivroModel.ts

---
+++ vue/src/features/biblioteca/types/apiLivroModel.ts
@@ -0,0 +1,33 @@
+export interface Pais {
+    id: number;
+    nome: string;
+}
+
+export interface Autor {
+    id: number;
+    nome: string;
+    ano_nascimento: number | null;
+    ano_obito: number | null;
+    pais: Pais;
+    pais_id: number | null;
+}
+
+export interface Genero {
+    id: number;
+    nome: string;
+}
+
+export interface Livro {
+    id: number;
+    titulo: string;
+    titulo_original: string | null;
+    autor: Autor;
+    genero: Genero;
+    ano_publicacao: number | null;
+    lido: boolean;
+    arquivo: string | null;
+    capa: string | null;
+	autor_id: Autor;
+    genero_id: Genero;
+
+}
