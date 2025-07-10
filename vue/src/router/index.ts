7489b260f471c506f15a18e907d03e2e1cbf27ca
JezielHonorato <jezielh.ss@gmail.com>
2025-07-09 23:34:15 -0300

    Update index.ts

--- vue/src/router/index.ts
+++ vue/src/router/index.ts
@@ -1,14 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import { LivrosRoutes } from '@/features/biblioteca/livros/routes';
 import Home from '@/components/Home.vue';
-import Livros from '@/components/Livros.vue';
-import LivrosCadastro from '@/components/LivrosCadastro.vue';
 
 const routes = [
-    { path: '/', name: 'Home', component: Home },
-    { path: '/livros', name: 'Livros', component: Livros },
-    { path: '/livros/cadastro', name: 'LivrosCadastro', component: LivrosCadastro },
-    { path: '/livros/editar/:id', name: 'LivrosEditar', component: LivrosCadastro, props: true },
-    { path: '/:catchAll(.*)', name: 'not-found', component: Home },
+    {
+        path: '/',
+        name: 'Home',
+        component: Home,
+    },
+    ...LivrosRoutes,
+    {
+        path: '/:catchAll(.*)',
+        name: 'not-found',
+        component: Home,
+    },
 ];
 
 const router = createRouter({
