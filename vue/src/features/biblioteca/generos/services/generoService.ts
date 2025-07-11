e0b1036687b1ab561c964ba0a261ea3aeca186ea
JezielHonorato <jezielh.ss@gmail.com>
2025-07-10 23:51:56 -0300

    Create generoService.ts

---
+++ vue/src/features/biblioteca/generos/services/generoService.ts
@@ -0,0 +1,10 @@
+import { CrudService } from '@/services/crudService';
+import type { Genero, GeneroForm } from '../models/generoModel';
+
+class GeneroService extends CrudService<Genero, GeneroForm> {
+    constructor() {
+        super('generos');
+    }
+}
+
+export const generoService = new GeneroService();
