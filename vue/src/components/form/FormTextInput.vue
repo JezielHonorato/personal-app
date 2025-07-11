3909149b6ce29706210c21fa75f8ba79a654e4d5
JezielHonorato <jezielh.ss@gmail.com>
2025-07-10 23:51:35 -0300

    Update FormTextInput.vue

--- vue/src/components/form/FormTextInput.vue
+++ vue/src/components/form/FormTextInput.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <label :for="id" class="block text-gray-800 text-sm font-semibold mb-2 dark:text-gray-200">{{ label }}</label>
-        <input type="text" :id="id" :placeholder="placeholder" v-model="internalValue" class="shadow-sm appearance-none border border-gray-300 rounded-md w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-200 ease-in-out dark:bg-gray-700 dark:border-gray-600 dark:text-gray-100 dark:placeholder-gray-400" />
+        <input type="text" :id="id" :placeholder="placeholder" v-model="inputValue" class="shadow-sm appearance-none border border-gray-300 rounded-md w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-200 ease-in-out dark:bg-gray-700 dark:border-gray-600 dark:text-gray-100 dark:placeholder-gray-400" />
     </div>
 </template>
 
@@ -9,15 +9,15 @@
     import { computed } from 'vue';
 
     const props = defineProps<{
+        id: string;
         label: string;
         placeholder?: string;
-        modelValue: string | number | null;
-        id: string;
+        modelValue: string | null;
     }>();
 
     const emit = defineEmits(['update:modelValue']);
 
-    const internalValue = computed({
+    const inputValue = computed({
         get: () => props.modelValue,
         set: (value) => emit('update:modelValue', value),
     });
