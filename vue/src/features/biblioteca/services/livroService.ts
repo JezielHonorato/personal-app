0f1d13cab5a70d718abeb992fa89a954e9e4dde3
JezielHonorato <jezielh.ss@gmail.com>
2025-07-12 17:35:39 -0300

    Create livroService.ts

---
+++ vue/src/features/biblioteca/services/livroService.ts
@@ -0,0 +1,16 @@
+import { api } from '@/config/axios';
+import { CrudService } from '@/services/crudService';
+import type { Livro, LivroForm, LivroConteudo } from '../models/livroModel';
+
+class LivroService extends CrudService<Livro, LivroForm> {
+    constructor() {
+        super('livros');
+    }
+
+    async getContent(id: number): Promise<LivroConteudo> {
+        const response = await api.get<LivroConteudo>(`/livros/${id}/conteudo`);
+        return response.data;
+    }
+}
+
+export const livroService = new LivroService();
