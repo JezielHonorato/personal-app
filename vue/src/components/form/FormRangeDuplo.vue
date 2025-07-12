e2ea20b1d9249a5407a82f6cbf0bc098a65d2a52
JezielHonorato <jezielh.ss@gmail.com>
2025-07-12 17:34:14 -0300

    Update FormRangeDuplo.vue

--- vue/src/components/form/FormRangeDuplo.vue
+++ vue/src/components/form/FormRangeDuplo.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="campos-pesquisa">
+    <div>
         <section class="range-value-input flex justify-between items-center mb-2">
             <p class="block text-sm font-medium text-gray-700 dark:text-gray-300">{{ label }}</p>
             <p class="valores-input text-sm text-gray-700 dark:text-gray-300 font-semibold">
@@ -10,7 +10,7 @@
         <section class="campos-input campo-range relative h-2 flex items-center">
             <div class="absolute w-full h-1 bg-gray-300 dark:bg-gray-600 rounded-full"></div>
             <div class="linha-progresso absolute h-1 rounded-full bg-indigo-500 dark:bg-indigo-400" :style="progressStyle"></div>
-            
+
             <input type="range" :min="min" :max="max" v-model.number="novoValorInicial" class="Periodo absolute w-full h-full appearance-none bg-transparent outline-none z-10" />
             <input type="range" :min="min" :max="max" v-model.number="novoValorFinal" class="Periodo absolute w-full h-full appearance-none bg-transparent outline-none z-10" />
         </section>
@@ -18,41 +18,44 @@
 </template>
 
 <script lang="ts" setup>
-import { computed } from 'vue';
+    import { computed } from 'vue';
 
-const props = defineProps<{
-    label: string;
-    min: number;
-    max: number;
-    valorInicial: number;
-    valorFinal: number;
-}>();
+    const props = defineProps<{
+        label: string;
+        min: number;
+        max: number;
+        valorInicial: number | null;
+        valorFinal: number | null;
+    }>();
 
-const emit = defineEmits(['update:valorInicial', 'update:valorFinal']);
+    const emit = defineEmits(['update:valorInicial', 'update:valorFinal']);
 
-const novoValorInicial = computed({
-    get: () => props.valorInicial,
-    set: (value) => emit('update:valorInicial', value)
-});
+    const novoValorInicial = computed({
+        get: () => props.valorInicial,
+        set: (value) => emit('update:valorInicial', value),
+    });
 
-const novoValorFinal = computed({
-    get: () => props.valorFinal,
-    set: (value) => emit('update:valorFinal', value)
-});
+    const novoValorFinal = computed({
+        get: () => props.valorFinal,
+        set: (value) => emit('update:valorFinal', value),
+    });
 
-const progressStyle = computed(() => {
-    const range = props.max - props.min;
-    const visualStart = Math.min(novoValorInicial.value, novoValorFinal.value);
-    const visualEnd = Math.max(novoValorInicial.value, novoValorFinal.value);
+    const progressStyle = computed(() => {
+        const range = props.max - props.min;
+        const visualStart = Math.min(novoValorInicial.value ?? 1, novoValorFinal.value ?? 2);
+        const visualEnd = Math.max(novoValorInicial.value ?? 1, novoValorFinal.value ?? 2);
 
-    const left = ((visualStart - props.min) / range) * 100;
-    const width = ((visualEnd - visualStart) / range) * 100;
+        const left = novoValorInicial.value && novoValorFinal.value ? ((visualStart - props.min) / range) * 100 : 50;
+        const width = novoValorInicial.value && novoValorFinal.value ? ((visualEnd - visualStart) / range) * 100 : 0;
 
-    return {
-        left: `${left}%`,
-        width: `${width}%`,
-    };
-});
+        if (novoValorInicial.value && !novoValorFinal.value) novoValorFinal.value = props.max;
+        if (!novoValorInicial.value && novoValorFinal.value) novoValorInicial.value = props.min;
+
+        return {
+            left: `${left}%`,
+            width: `${width}%`,
+        };
+    });
 </script>
 
 <style scoped>
