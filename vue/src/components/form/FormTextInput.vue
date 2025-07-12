2ca1ddeb34665dcc4aee9e87ccd2b811fdaeec4e
JezielHonorato <jezielh.ss@gmail.com>
2025-07-12 17:34:06 -0300

    Update FormTextInput.vue

--- vue/src/components/form/FormTextInput.vue
+++ vue/src/components/form/FormTextInput.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <label :for="id" class="block text-gray-800 text-sm font-semibold mb-2 dark:text-gray-200">{{ label }}</label>
+        <label :for="id" class="block text-gray-800 text-sm font-semibold mb-2 dark:text-gray-200">{{ label }}:</label>
         <input type="text" :id="id" :placeholder="placeholder" v-model="inputValue" class="shadow-sm appearance-none border border-gray-300 rounded-md w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-200 ease-in-out dark:bg-gray-700 dark:border-gray-600 dark:text-gray-100 dark:placeholder-gray-400" />
     </div>
 </template>
