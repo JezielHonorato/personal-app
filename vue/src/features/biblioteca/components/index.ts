492c814244d45d67cb597a50f9f5293e72a733e9
JezielHonorato <jezielh.ss@gmail.com>
2025-07-12 17:34:30 -0300

    Create index.ts

---
+++ vue/src/features/biblioteca/components/index.ts
@@ -0,0 +1,8 @@
+export { default as AutorCadastro } from '../components/AutorCadastro.vue';
+export { default as GeneroCadastro } from '../components/GeneroCadastro.vue';
+export { default as Livro } from '../components/Livro.vue';
+export { default as LivroCadastro } from '../components/LivroCadastro.vue';
+export { default as LivroConteudo } from '../components/LivroConteudo.vue';
+export { default as LivroPesquisa } from '../components/LivroPesquisa.vue';
+export { default as LivroResultado } from '../components/LivroResultado.vue';
+export { default as PaisCadastro } from '../components/PaisCadastro.vue';
