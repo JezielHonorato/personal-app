259717bf7d7f1aab92e109510cbc27374d7497aa
JezielHonorato <jezielh.ss@gmail.com>
2025-07-12 17:35:25 -0300

    Create index.ts

---
+++ vue/src/features/biblioteca/services/index.ts
@@ -0,0 +1,4 @@
+export { autorService } from './autorService';
+export { livroService } from './livroService';
+export { generoService } from './generoService';
+export { paisService } from './paisService';
