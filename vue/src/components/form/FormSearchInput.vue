e2a7fe06992baa62f7b82c8162b8ef44b195d14b
JezielHonorato <jezielh.ss@gmail.com>
2025-07-12 17:34:08 -0300

    Update FormSearchInput.vue

--- vue/src/components/form/FormSearchInput.vue
+++ vue/src/components/form/FormSearchInput.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <label :for="id" class="block text-gray-800 text-sm font-semibold mb-2 dark:text-gray-200">{{ label }}</label>
+        <label :for="id" class="block text-gray-800 text-sm font-semibold mb-2 dark:text-gray-200"> {{ label }}: </label>
         <div class="campos-input relative flex items-center">
             <Search class="material-icons absolute left-3 text-gray-400 dark:text-gray-500" />
             <input type="search" :id="id" :placeholder="placeholder" v-model="inputValue" autocomplete="off" class="pl-10 shadow-sm appearance-none border border-gray-300 rounded-md w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-200 ease-in-out dark:bg-gray-700 dark:border-gray-600 dark:text-gray-100 dark:placeholder-gray-400" />
