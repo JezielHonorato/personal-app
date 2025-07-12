6f74e7d13c95c149dacd1f48e88379c96ef345d4
JezielHonorato <jezielh.ss@gmail.com>
2025-07-12 17:35:24 -0300

    Update FormNumberInput.vue

--- vue/src/components/form/FormNumberInput.vue
+++ vue/src/components/form/FormNumberInput.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <label :for="id" class="block text-sm font-semibold mb-2 dark:text-gray-200 text-gray-800">{{ label }}</label>
+        <label :for="id" class="block text-sm font-semibold mb-2 dark:text-gray-200 text-gray-800"> {{ label }}: </label>
         <input type="number" :id="id" :placeholder="placeholder" v-model="inputValue" class="shadow-sm border border-gray-300 rounded-md w-full py-2 px-3 text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:text-gray-100" />
     </div>
 </template>
