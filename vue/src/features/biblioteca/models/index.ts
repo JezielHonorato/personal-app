711f3cc8df69413b6e8f7a22840503d8627aeee3
JezielHonorato <jezielh.ss@gmail.com>
2025-07-12 17:36:03 -0300

    Create index.ts

---
+++ vue/src/features/biblioteca/models/index.ts
@@ -0,0 +1,4 @@
+export type { Autor, AutorForm } from './autorModel';
+export type { Livro, LivroConteudo, LivroFiltro, LivroForm } from './livroModel';
+export type { Genero, GeneroForm } from './generoModel';
+export type { Pais, PaisForm } from './paisModel';
