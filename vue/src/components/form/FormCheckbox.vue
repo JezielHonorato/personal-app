26cbcc94c8d2750dd59b8bed84aad958e0284478
JezielHonorato <jezielh.ss@gmail.com>
2025-07-11 23:23:01 -0300

    Update FormCheckbox.vue

--- vue/src/components/form/FormCheckbox.vue
+++ vue/src/components/form/FormCheckbox.vue
@@ -1,16 +1,23 @@
 <template>
     <div class="flex items-center">
-        <input type="checkbox" :id="id" :checked="modelValue" @change="$emit('update:modelValue', ($event.target as HTMLInputElement).checked)" class="mr-2" />
+        <input type="checkbox" :id="id" v-model="checkedValue" class="mr-2 h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500" />
         <label :for="id" class="text-sm font-semibold text-gray-800 dark:text-gray-200">{{ label }}</label>
     </div>
 </template>
 
 <script setup lang="ts">
-    defineProps<{
+    import { computed } from 'vue';
+
+    const props = defineProps<{
         id: string;
         label: string;
         modelValue: boolean;
     }>();
-	
-    defineEmits(['update:modelValue']);
+
+    const emit = defineEmits(['update:modelValue']);
+
+    const checkedValue = computed({
+        get: () => props.modelValue,
+        set: (value) => emit('update:modelValue', value),
+    });
 </script>
