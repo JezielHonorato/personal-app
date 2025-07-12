9f1ca7f2dab206c548145b3a4183b044d564042c
JezielHonorato <jezielh.ss@gmail.com>
2025-07-12 17:36:12 -0300

    Create FormCancelButton.vue

---
+++ vue/src/components/form/FormCancelButton.vue
@@ -0,0 +1,13 @@
+<template>
+    <button type="button" @click="voltar" class="w-full sm:w-auto bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-6 rounded-lg">{{ texto ?? 'Cancelar' }}</button>
+</template>
+
+<script setup lang="ts">
+    defineProps<{
+        texto?: string;
+    }>();
+
+    const voltar = (): void => {
+        history.back();
+    };
+</script>
