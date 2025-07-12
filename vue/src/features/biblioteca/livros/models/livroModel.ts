b660268cbe1650c5067906a0ba28c82c39e81daa
JezielHonorato <jezielh.ss@gmail.com>
2025-07-11 23:22:17 -0300

    Update livroModel.ts

--- vue/src/features/biblioteca/livros/models/livroModel.ts
+++ vue/src/features/biblioteca/livros/models/livroModel.ts
@@ -1,7 +1,7 @@
-import type { IAutor } from '@/features/biblioteca/autores/models/autorModel';
-import type { IGenero } from '@/features/biblioteca/generos/models/generoModel';
+import type { Autor } from '@/features/biblioteca/autores/models/autorModel';
+import type { Genero } from '@/features/biblioteca/generos/models/generoModel';
 
-export interface ILivro {
+export interface Livro {
     id: number;
     titulo: string;
     titulo_original: string | null;
@@ -10,6 +10,31 @@ export interface ILivro {
     arquivo: string | null;
     capa: string | null;
 
-    autor: IAutor;
-    genero: IGenero;
+    autor: Autor | null;
+    genero: Genero | null;
+}
+
+export interface LivroForm {
+    id: number | null;
+    titulo: string;
+    titulo_original: string | null;
+    ano_publicacao: number | null;
+    lido: boolean;
+    autorId: number | null;
+    generoId: number | null;
+    arquivo_file: File | null;
+    capa_file: File | null;
+    capa_preview_url: string | null;
+}
+
+export interface LivroFiltro {
+    tituloAutor: string | null;
+    generoId: number | null;
+    paisId: number | null;
+    lido: boolean | null;
+}
+export interface LivroConteudo {
+    type: 'txt' | 'md' | 'pdf';
+    content?: string;
+    url?: string;
 }
