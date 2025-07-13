c30faf2d81fb619f6330e543fe923cbb23481530
JezielHonorato <jezielh.ss@gmail.com>
2025-07-13 20:22:10 -0300

    Update LivroPesquisa.vue

--- vue/src/features/biblioteca/components/LivroPesquisa.vue
+++ vue/src/features/biblioteca/components/LivroPesquisa.vue
@@ -6,25 +6,25 @@
 
         <FormSelect class="flex-grow" id="pais" label="País" placeholder="Todos os Países" :options="paises" v-model="selectPais" :carregando="carregandoPaises" :erro="erroPaises" />
 
-        <FormRangeDuplo label="Período:" :min="anoMin" :max="anoMax" v-model:valorInicial="rangeAnoInicial" v-model:valorFinal="rangeAnoFinal" />
+        <FormRangeDuplo label="Período" :min="props.anoMin" :max="props.anoMax" v-model:valorInicial="rangeAnoInicial" v-model:valorFinal="rangeAnoFinal" />
     </section>
 </template>
 
 <script lang="ts" setup>
-    import { ref, watch, onMounted } from 'vue';
+    import { ref, watch, onMounted, type Ref } from 'vue';
     import { FormSelect, FormRangeDuplo, FormSearchInput } from '@/components/form/';
     import { useGenero, usePais } from '../composables';
+
+    const props = defineProps<{ anoMin: number; anoMax: number }>();
+
     const emit = defineEmits(['pesquisa']);
 
-    const queryNome = ref<string>('');
-    const selectGenero = ref<number | null>(null);
-    const selectPais = ref<number | null>(null);
+    const queryNome: Ref<string> = ref<string>('');
+    const selectGenero: Ref<number | null> = ref<number | null>(null);
+    const selectPais: Ref<number | null> = ref<number | null>(null);
 
-    const anoMin = 1800;
-    const anoMax = new Date().getFullYear();
-
-    const rangeAnoInicial = ref<number | null>(null);
-    const rangeAnoFinal = ref<number | null>(null);
+    const rangeAnoInicial: Ref<number | null> = ref<number | null>(null);
+    const rangeAnoFinal: Ref<number | null> = ref<number | null>(null);
     const { generos, carregando: carregandoGeneros, erro: erroGeneros, getGeneros } = useGenero();
     const { paises, carregando: carregandoPaises, erro: erroPaises, getPaises } = usePais();
 
