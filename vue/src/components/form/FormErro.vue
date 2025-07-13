77293c9c0bebc5f76e581d5aff31ea03586c4f9a
JezielHonorato <jezielh.ss@gmail.com>
2025-07-13 20:21:29 -0300

    Create FormErro.vue

---
+++ vue/src/components/form/FormErro.vue
@@ -0,0 +1,11 @@
+<template>
+    <p v-if="erro" class="text-red-600 dark:text-red-400 text-sm mt-6 text-center font-medium bg-red-100 dark:bg-red-900/40 p-2 rounded">
+        {{ erro }}
+    </p>
+</template>
+
+<script setup lang="ts">
+    defineProps<{
+        erro: string | null;
+    }>();
+</script>
