2b754cddfc075bf6cfebe3cb8256a9bff92b3f3f
JezielHonorato <jezielh.ss@gmail.com>
2025-07-18 13:36:49 -0300

    Create 003_autor.php

---
+++ laravel/database/migrations/livros/003_autor.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+	public function up(): void
+	{
+		Schema::create('autor', function (Blueprint $table) {
+			$table->id();
+			$table->string('nome', 255);
+			$table->integer('ano_nascimento')->nullable();
+			$table->integer('ano_obito')->nullable();
+			$table->foreignId('pais_id')->nullable()->constrained('pais')->nullOnDelete();
+		});
+	}
+
+	public function down(): void
+	{
+		Schema::dropIfExists('autor');
+	}
+};
