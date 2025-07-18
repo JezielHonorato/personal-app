ea940c329179b1c026c1de899a7d66b4b2a5ffdd
JezielHonorato <jezielh.ss@gmail.com>
2025-07-18 13:37:11 -0300

    Create Livro.php

---
+++ laravel/app/Models/Livro.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Livro extends Model
+{
+	protected $table = 'livro';
+	protected $fillable = [
+		'titulo',
+		'titulo_original',
+		'autor_id',
+		'genero_id',
+		'ano_publicacao',
+		'lido',
+		'arquivo',
+		'capa',
+	];
+
+	public function autor()
+	{
+		return $this->belongsTo(Autor::class);
+	}
+
+	public function genero()
+	{
+		return $this->belongsTo(Genero::class);
+	}
+}
