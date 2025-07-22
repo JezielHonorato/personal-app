b4d1bdf78182680027ed2b5dee04679d36ec9713
JezielHonorato <jezielh.ss@gmail.com>
2025-07-21 22:48:42 -0300

    Update validators.ts

--- vue/src/utils/validators.ts
+++ vue/src/utils/validators.ts
@@ -11,6 +11,6 @@ export function validarPreenchimento(entrada: any): boolean {
 }
 
 export function validarNome(string: string): boolean {
-    const nameRegex = /^(?=.*[a-zA-Z])[a-zA-Z .]+$/;
+    const nameRegex: RegExp = /^(?=.*[a-zA-Z])[a-zA-Z .]+$/;
     return nameRegex.test(string);
 }
