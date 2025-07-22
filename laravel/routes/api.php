e9ca9bc8cbe0896d93bd51ad454756cbeb679c92
JezielHonorato <jezielh.ss@gmail.com>
2025-07-21 23:03:03 -0300

    Update api.php

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -1,19 +1,13 @@
 <?php
 
-use App\Http\Controllers\LivrosController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\PaisController;
-use App\Http\Controllers\AutorController;
-use App\Http\Controllers\GeneroController;
-use App\Http\Controllers\LivroController;
+use App\DatabaseLivros\Http\Controllers\PaisController;
+use App\DatabaseLivros\Http\Controllers\AutorController;
+use App\DatabaseLivros\Http\Controllers\GeneroController;
+use App\DatabaseLivros\Http\Controllers\LivroController;
 
-Route::apiResource('pais', PaisController::class);
+Route::apiResource('paises', PaisController::class);
 Route::apiResource('autores', AutorController::class);
 Route::apiResource('generos', GeneroController::class);
 Route::apiResource('livros', LivroController::class);
-
-Route::get('/user', function (Request $request) {
-	return $request->user();
-})->middleware('auth:sanctum');
