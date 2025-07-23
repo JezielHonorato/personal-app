7e1173c3c5b47c8ea162674efbb0fad721bd264a
JezielHonorato <jezielh.ss@gmail.com>
2025-07-22 22:17:39 -0300

    Create Autor.php

---
+++ laravel/app/DatabaseLivros/Models/Autor.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\DatabaseLivros\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Autor extends Model
+{
+	protected $table = 'autor';
+	protected $fillable = [
+		'nome',
+		'ano_nascimento',
+		'ano_obito',
+		'pais_id',
+	];
+
+	public function pais()
+	{
+		return $this->belongsTo(Pais::class);
+	}
+
+	public function livrosDoAutor()
+	{
+		return $this->hasMany(Livro::class, 'autor_id');
+	}
+}
