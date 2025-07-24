8253fe47862b075f33a93ff924d12b231814f9d5
JezielHonorato <jezielh.ss@gmail.com>
2025-07-23 22:58:16 -0300

    Update LivroController.php

--- laravel/app/DatabaseLivros/Http/Controllers/LivroController.php
+++ laravel/app/DatabaseLivros/Http/Controllers/LivroController.php
@@ -4,69 +4,67 @@ namespace App\DatabaseLivros\Http\Controllers;
 
 use App\DatabaseLivros\Http\Requests\StoreLivroRequest;
 use App\DatabaseLivros\Http\Requests\UpdateLivroRequest;
+use App\DatabaseLivros\Models\Livro;
 use App\Helpers\ApiResponse;
-use Illuminate\Support\Facades\DB;
 
 class LivroController
 {
-	public function index()
-	{
-		$livros = DB::table('livro')->get();
+    public function index()
+    {
+        $livros = Livro::with(['autor', 'genero'])
+            ->get();
 
-		return ApiResponse::success($livros, 201);
-	}
+        return ApiResponse::success($livros, 201);
+    }
 
-	public function show($id)
-	{
-		$livro = DB::table('livro')
-			->where('id', $id)
-			->first();
+    public function show($id)
+    {
+        $livro = Livro::with(['autor', 'genero'])
+            ->findOrFail($id);
 
-		return ApiResponse::success($livro, 201);
-	}
+        return ApiResponse::success($livro, 201);
+    }
 
-	public function store(StoreLivroRequest $request)
-	{
-		$validated = $request->validated();
+    public function store(StoreLivroRequest $request)
+    {
+        $validated = $request->validated();
 
-		if ($request->hasFile('arquivo')) {
-			$validated['arquivo'] = $request->file('arquivo')->store('livros/arquivos', 'assets_externo');
-		}
+        if ($request->hasFile('arquivo')) {
+            $validated['arquivo'] = $request->file('arquivo')->store('livros/arquivos', 'assets_externo');
+        }
 
-		if ($request->hasFile('capa')) {
-			$validated['capa'] = $request->file('capa')->store('livros/capas', 'assets_externo');
-		}
+        if ($request->hasFile('capa')) {
+            $validated['capa'] = $request->file('capa')->store('livros/capas', 'assets_externo');
+        }
 
-		DB::table('livro')->insert($validated);
+        Livro::create($validated);
 
-		return ApiResponse::success(null, 201);
-	}
+        return ApiResponse::success(null, 201);
+    }
 
-	public function update(UpdateLivroRequest $request, int $id)
-	{
-		$validated = $request->validated();
+    public function update(UpdateLivroRequest $request, int $id)
+    {
+        $validated = $request->validated();
 
-		if ($request->hasFile('arquivo')) {
-			$validated['arquivo'] = $request->file('arquivo')->store('arquivos', 'livros_externo');
-		}
+        if ($request->hasFile('arquivo')) {
+            $validated['arquivo'] = $request->file('arquivo')->store('livros/arquivos', 'assets_externo');
+        }
 
-		if ($request->hasFile('capa')) {
-			$validated['capa'] = $request->file('capa')->store('capas', 'livros_externo');
-		}
+        if ($request->hasFile('capa')) {
+            $validated['capa'] = $request->file('capa')->store('livros/capas', 'assets_externo');
+        }
 
-		DB::table('livro')
-			->where('id', $id)
-			->update($validated);
+        Livro::findOrFail($id)
+            ->update($validated);
 
-		return ApiResponse::success(null, 201);
-	}
+        return ApiResponse::success(null, 201);
+    }
 
-	public function destroy(int $id)
-	{
-		DB::table('livro')
-			->where('id', $id)
-			->delete();
+    public function destroy(int $id)
+    {
+        Livro::findOrFail($id)
+            ->delete();
 
-		return ApiResponse::success(null, 204);
-	}
+        return ApiResponse::success(null, 204);
+    }
 }
