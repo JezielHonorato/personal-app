c921be73800121cefcdea6fe541e0df0754063d3
JezielHonorato <jezielh.ss@gmail.com>
2025-07-23 22:57:25 -0300

    Update 003_autor.php

--- laravel/database/migrations/livros/003_autor.php
+++ laravel/database/migrations/livros/003_autor.php
@@ -6,19 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-	public function up(): void
-	{
-		Schema::create('autor', function (Blueprint $table) {
-			$table->id();
-			$table->string('nome', 255);
-			$table->integer('ano_nascimento')->nullable();
-			$table->integer('ano_obito')->nullable();
-			$table->foreignId('pais_id')->nullable()->constrained('pais')->nullOnDelete();
-		});
-	}
+    public function up(): void
+    {
+        Schema::create('autor', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome', 255);
+            $table->integer('ano_nascimento')->nullable();
+            $table->integer('ano_obito')->nullable();
+            $table->foreignId('pais_id')->nullable()->constrained('pais')->nullOnDelete();
+        });
+    }
 
-	public function down(): void
-	{
-		Schema::dropIfExists('autor');
-	}
+    public function down(): void
+    {
+        Schema::dropIfExists('autor');
+    }
 };
