0a0dc52200213b1598726e602fe0c6cfc13880d8
JezielHonorato <jezielh.ss@gmail.com>
2025-07-23 22:58:03 -0300

    Update Livro.php

--- laravel/app/DatabaseLivros/Models/Livro.php
+++ laravel/app/DatabaseLivros/Models/Livro.php
@@ -6,25 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class Livro extends Model
 {
-	protected $table = 'livro';
-	protected $fillable = [
-		'titulo',
-		'titulo_original',
-		'autor_id',
-		'genero_id',
-		'ano_publicacao',
-		'lido',
-		'arquivo',
-		'capa',
-	];
+    protected $table = 'livro';
 
-	public function autor()
-	{
-		return $this->belongsTo(Autor::class);
-	}
+    protected $fillable = [
+        'titulo',
+        'titulo_original',
+        'autor_id',
+        'genero_id',
+        'ano_publicacao',
+        'lido',
+        'arquivo',
+        'capa',
+    ];
 
-	public function genero()
-	{
-		return $this->belongsTo(Genero::class);
-	}
+    public function autor()
+    {
+        return $this->belongsTo(Autor::class);
+    }
+
+    public function genero()
+    {
+        return $this->belongsTo(Genero::class);
+    }
 }
