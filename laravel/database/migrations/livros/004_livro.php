64d03e34345c3327f1333b9d2466ac6ac859fb18
JezielHonorato <jezielh.ss@gmail.com>
2025-07-23 22:57:23 -0300

    Update 004_livro.php

--- laravel/database/migrations/livros/004_livro.php
+++ laravel/database/migrations/livros/004_livro.php
@@ -6,23 +6,23 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-	public function up(): void
-	{
-		Schema::create('livro', function (Blueprint $table) {
-			$table->id();
-			$table->string('titulo', 255);
-			$table->string('titulo_original', 255)->nullable();
-			$table->foreignId('autor_id')->nullable()->constrained('autor')->nullOnDelete();
-			$table->foreignId('genero_id')->nullable()->constrained('genero')->nullOnDelete();
-			$table->integer('ano_publicacao')->nullable();
-			$table->boolean('lido')->default(false);
-			$table->string('arquivo')->nullable();
-			$table->string('capa')->nullable();
-		});
-	}
+    public function up(): void
+    {
+        Schema::create('livro', function (Blueprint $table) {
+            $table->id();
+            $table->string('titulo', 255);
+            $table->string('titulo_original', 255)->nullable();
+            $table->foreignId('autor_id')->nullable()->constrained('autor')->nullOnDelete();
+            $table->foreignId('genero_id')->nullable()->constrained('genero')->nullOnDelete();
+            $table->integer('ano_publicacao')->nullable();
+            $table->boolean('lido')->default(false);
+            $table->string('arquivo')->nullable();
+            $table->string('capa')->nullable();
+        });
+    }
 
-	public function down(): void
-	{
-		Schema::dropIfExists('livro');
-	}
+    public function down(): void
+    {
+        Schema::dropIfExists('livro');
+    }
 };
