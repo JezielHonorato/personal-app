256ca412a57625fb573fc4cd7f89ffccc2188f32
JezielHonorato <jezielh.ss@gmail.com>
2025-07-23 22:58:13 -0300

    Update PaisController.php

--- laravel/app/DatabaseLivros/Http/Controllers/PaisController.php
+++ laravel/app/DatabaseLivros/Http/Controllers/PaisController.php
@@ -2,60 +2,55 @@
 
 namespace App\DatabaseLivros\Http\Controllers;
 
+use App\DatabaseLivros\Models\Pais;
 use App\Helpers\ApiResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Validation\Rule;
 
 class PaisController
 {
-	public function index()
-	{
-		$paises = DB::table('pais')->get();
-		return ApiResponse::success($paises, 200);
-	}
+    public function index()
+    {
+        $paises = Pais::all();
 
-	public function show(int $id)
-	{
-		$pais = DB::table('pais')
-			->where('id', $id)
-			->first();
+        return ApiResponse::success($paises, 200);
+    }
 
-		return ApiResponse::success($pais, 200);
-	}
+    public function show(int $id)
+    {
+        $pais = Pais::findOrFail($id);
 
+        return ApiResponse::success($pais, 200);
+    }
 
-	public function store(Request $request)
-	{
-		$validated = $request->validate([
-			'nome' => ['required', 'string', 'max:50', 'regex:/^(?=.*\p{L})[\p{L} .]+$/u',  'unique:pais,nome'],
-		]);
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'nome' => ['required', 'string', 'max:50', 'regex:/^(?=.*\p{L})[\p{L} .]+$/u',  'unique:pais,nome'],
+        ]);
 
-		DB::table('pais')->insert($validated);
+        Pais::create($validated);
 
-		return ApiResponse::success(null, 201);
-	}
+        return ApiResponse::success(null, 201);
+    }
 
+    public function update(Request $request, int $id)
+    {
+        $validated = $request->validate([
+            'nome' => ['required', 'string', 'max:50', 'regex:/^(?=.*\p{L})[\p{L} .]+$/u', Rule::unique('pais')->ignore($id)],
+        ]);
 
-	public function update(Request $request, int $id)
-	{
-		$validated = $request->validate([
-			'nome' => ['required', 'string', 'max:50', 'regex:/^(?=.*\p{L})[\p{L} .]+$/u', Rule::unique('pais')->ignore($id)],
-		]);
+        Pais::findOrFail($id)
+            ->update($validated);
 
-		DB::table('pais')
-			->where('id', $id)
-			->update($validated);
+        return ApiResponse::success(null, 201);
+    }
 
-		return ApiResponse::success(null, 201);
-	}
+    public function destroy(int $id)
+    {
+        Pais::findOrFail($id)
+            ->delete();
 
-	public function destroy(int $id)
-	{
-		DB::table('pais')
-			->where('id', $id)
-			->delete();
-
-		return ApiResponse::success(null, 204);
-	}
+        return ApiResponse::success(null, 204);
+    }
 }
