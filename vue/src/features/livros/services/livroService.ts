dc8ff9f5600aa00f5d968c4acc1181b6164f0829
JezielHonorato <jezielh.ss@gmail.com>
2025-07-27 22:14:33 -0300

    Update livroService.ts

--- vue/src/features/livros/services/livroService.ts
+++ vue/src/features/livros/services/livroService.ts
@@ -1,6 +1,6 @@
 import { api } from '@/config/axios';
 import { CrudService } from '@/services/crudService';
-import type { Livro, LivroForm, LivroConteudo } from '../models/livroModel';
+import type { Livro, LivroConteudo } from '../models/livroModel';
 
 class LivroService extends CrudService<Livro> {
     constructor() {
