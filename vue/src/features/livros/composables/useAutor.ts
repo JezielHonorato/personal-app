3d4e13cbcc48f8682c8b830f74f65fe3d6c27453
JezielHonorato <jezielh.ss@gmail.com>
2025-07-27 22:14:44 -0300

    Update useAutor.ts

--- vue/src/features/livros/composables/useAutor.ts
+++ vue/src/features/livros/composables/useAutor.ts
@@ -1,16 +1,16 @@
 import { ref, type Ref } from 'vue';
 import { autorService } from '../services';
-import type { Autor, AutorForm } from '../models';
+import type { Autor } from '../models';
 
 export function useAutor() {
     const autor: Ref<Autor | null> = ref<Autor | null>(null);
     const autores: Ref<Autor[]> = ref<Autor[]>([]);
     const carregando: Ref<boolean> = ref(false);
-    const erro: Ref<string | null> = ref<string | null>(null);
+    const erro: Ref<string[]> = ref<string[]>([]);
 
     async function getAutores(): Promise<void> {
         carregando.value = true;
-        erro.value = null;
+        erro.value = [];
         try {
             autores.value = await autorService.getAll();
         } catch (err: any) {
@@ -23,7 +23,7 @@ export function useAutor() {
 
     async function getAutor(id: number): Promise<void> {
         carregando.value = true;
-        erro.value = null;
+        erro.value = [];
         try {
             autor.value = await autorService.getById(id);
         } catch (err: any) {
@@ -34,9 +34,9 @@ export function useAutor() {
         }
     }
 
-    async function createAutor(data: AutorForm): Promise<void> {
+    async function createAutor(data: FormData): Promise<void> {
         carregando.value = true;
-        erro.value = null;
+        erro.value = [];
         try {
             await autorService.create(data);
         } catch (err: any) {
@@ -47,9 +47,9 @@ export function useAutor() {
         }
     }
 
-    async function updateAutor(id: number, data: AutorForm): Promise<void> {
+    async function updateAutor(id: number, data: FormData): Promise<void> {
         carregando.value = true;
-        erro.value = null;
+        erro.value = [];
         try {
             await autorService.update(id, data);
         } catch (err: any) {
@@ -62,7 +62,7 @@ export function useAutor() {
 
     async function deleteAutor(id: number): Promise<void> {
         carregando.value = true;
-        erro.value = null;
+        erro.value = [];
         try {
             await autorService.delete(id);
         } catch (err: any) {
