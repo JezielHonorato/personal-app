a9cdda8f9c34f120a14746cc79d5e58ea7278348
JezielHonorato <jezielh.ss@gmail.com>
2025-07-27 22:15:23 -0300

    Update FormErro.vue

--- vue/src/components/form/FormErro.vue
+++ vue/src/components/form/FormErro.vue
@@ -1,11 +1,13 @@
 <template>
-    <p v-if="erro" class="text-red-600 dark:text-red-400 text-sm mt-6 text-center font-medium bg-red-100 dark:bg-red-900/40 p-2 rounded">
-        {{ erro }}
-    </p>
+    <div v-if="erro && erro.length" class="text-red-600 dark:text-red-400 text-sm mt-6 text-center font-medium bg-red-100 dark:bg-red-900/40 p-2 rounded">
+        <p v-for="(mensagem, index) in erro" :key="index" class="mb-1">
+            {{ mensagem }}
+        </p>
+    </div>
 </template>
 
 <script setup lang="ts">
     defineProps<{
-        erro: string | null;
+        erro: string[] | null;
     }>();
 </script>
