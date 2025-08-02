7c51483fa17dcef728f033440f31b81983230883
JezielHonorato <jezielh.ss@gmail.com>
2025-08-02 13:28:40 -0300

    Update FormFileInput.vue

--- vue/src/components/form/FormFileInput.vue
+++ vue/src/components/form/FormFileInput.vue
@@ -1,12 +1,12 @@
 <template>
     <div>
         <label :for="id" class="block text-sm font-semibold mb-2 text-gray-800 dark:text-gray-200"> {{ label }}: </label>
-        <input type="file" :id="id" :accept="tipoArquivo" @change="atualizar" class="block w-full text-sm text-gray-700 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-blue-100 file:text-blue-700 hover:file:bg-blue-200 dark:file:bg-blue-800 dark:file:text-blue-200 dark:hover:file:bg-blue-700" />
+        <input type="file" :id="id" :accept="accept" @change="atualizar" class="block w-full text-sm text-gray-700 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-blue-100 file:text-blue-700 hover:file:bg-blue-200 dark:file:bg-blue-800 dark:file:text-blue-200 dark:hover:file:bg-blue-700" />
 
         <div v-if="previewUrl || fileName" class="mt-4 p-3 border rounded-lg bg-gray-50 dark:bg-gray-700 flex items-center justify-between">
             <div class="flex items-center">
                 <img v-if="previewUrl" :src="previewUrl" alt="Pré-visualização do arquivo" class="w-16 h-16 object-cover rounded mr-4 border border-gray-200 dark:border-gray-600" />
-                <div v-if="fileName" class="text-sm text-gray-700 dark:text-gray-300">
+                <div v-if="fileName" class="text-sm break-all text-gray-700 dark:text-gray-300">
                     <p class="font-semibold break-words">{{ fileName }}</p>
                     <p v-if="fileSize">{{ (fileSize / 1024).toFixed(2) }} KB</p>
                 </div>
@@ -25,12 +25,11 @@
     const props = defineProps<{
         id: string;
         label: string;
-        tipoArquivo?: string;
+        accept?: string;
+        modelValue: File | null;
     }>();
 
-    const emit = defineEmits<{
-        (e: 'change', file: File | null): void;
-    }>();
+    const emit = defineEmits(['update:modelValue']);
 
     const previewUrl = ref<string | null>(null);
     const fileName = ref<string | null>(null);
@@ -57,17 +56,17 @@
             limparPreview();
         }
 
-        emit('change', file);
+        emit('update:modelValue', file);
     }
 
-    const  removerArquivo =() => {
+    const removerArquivo = () => {
         const inputElement = document.getElementById(props.id) as HTMLInputElement;
         if (inputElement) {
             inputElement.value = '';
         }
         limparPreview();
-        emit('change', null);
-    }
+        emit('update:modelValue', null);
+    };
 
     function limparPreview() {
         previewUrl.value = null;
